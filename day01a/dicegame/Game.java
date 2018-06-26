04511192bcc2487bf608c502ecdc04354c93dae0
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-26 14:19:33 -0500

    Scripted auto-commit on change (2018-06-26 14:19:32) by gitwatch.sh

--- day01a/dicegame/Game.java
+++ day01a/dicegame/Game.java
@@ -5,9 +5,10 @@ public class Game
     {
         Die die1 = new Die();
         die1.roll();
-        System.out.println(die1.getShowing();
+        System.out.println(die1.getShowing());
         Die die2 = new Die();
         die2.roll();
+        System.out.println(die1.getShowing());
         
     }
 }
