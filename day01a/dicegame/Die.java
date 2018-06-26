682a0b26321a72c4283de0aaf2fce4c5a143c952
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-26 15:09:51 -0500

    Scripted auto-commit on change (2018-06-26 15:09:50) by gitwatch.sh

---
+++ day01a/dicegame/Die.java
@@ -0,0 +1,42 @@
+
+public class Die
+{
+    // instance variables
+    private int showing;
+    private int numSides;
+    
+    // constructor: same name as class
+    // automatically runs when object is created
+    public Die(int sidesToSet)
+    {
+        numSides = sidesToSet;
+        roll();
+    }
+
+     public Die(int sidesToSet)
+    {
+        numSides = sidesToSet;
+        roll();
+    }
+
+    public void roll()
+    {
+        showing = (int)(Math.random()*numSides+1);
+    }
+    
+    public int getShowing()
+    {
+        return showing;
+    }
+    
+    public void setNumSides(int sidesToSet)
+    {
+        numSides = sidesToSet;
+    }
+
+    /*public void setNumSides(int numSides)
+    {
+        this.numSides = numSides;
+    }*/
+    
+}
