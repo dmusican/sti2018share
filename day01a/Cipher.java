cc1291f3a3fd803b28bffdd320f4a2a652d6b54e
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-26 13:27:33 -0500

    Scripted auto-commit on change (2018-06-26 13:27:32) by gitwatch.sh

--- day01a/Cipher.java
+++ day01a/Cipher.java
@@ -6,6 +6,7 @@ public class Cipher {
   public static void main(String[] main)
   {
     System.out.print("What is the word to be encoded? ");
+    Scanner inp = new Scanner(System.in);
     
   }
   
