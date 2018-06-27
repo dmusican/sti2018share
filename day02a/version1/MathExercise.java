c41b95d1f6267e93dadfb184194a157cab904ea9
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-27 09:42:33 -0500

    Scripted auto-commit on change (2018-06-27 09:42:33) by gitwatch.sh

--- day02a/version1/MathExercise.java
+++ day02a/version1/MathExercise.java
@@ -5,7 +5,8 @@ public class MathExercise {
       Fraction frac1 = new Fraction(3,5);
       Fraction frac2 = new Fraction(4,7);
       frac1.display();
-      Fraction answer = Fraction.multiply(frac1, frac2);
+      Fraction answer = 
+           Fraction.multiply(frac1, frac2);
       answer.display();
       System.out.println(Math.PI);
    }
