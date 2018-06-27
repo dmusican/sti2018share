46dd63980dfd50b90c22a3f35d26a0f9a3592b4f
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-27 13:27:22 -0500

    Scripted auto-commit on change (2018-06-27 13:27:21) by gitwatch.sh

--- day02a/inheritance/Circle.java
+++ day02a/inheritance/Circle.java
@@ -1,3 +1,15 @@
 public class Circle {
    private double radius;
+   
+   public Circle() {
+      radius = 3;
+   }
+   
+   public double area() {
+      return Math.PI * Math.pow(radius,2);
+   }
+   
+   public double perimeter() {
+      return 2 * Math.PI * radius;
+   }
 }
--- day02a/inheritance/Geometry.java
+++ day02a/inheritance/Geometry.java
@@ -1,5 +1,7 @@
 public class Geometry {
    public static void main(String[] args) {
       Circle c = new Circle();
+      System.out.println(c.area());
+      System.out.println(c.perimeter());
    }
 }
