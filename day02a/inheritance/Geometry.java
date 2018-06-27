2db51a9e83f485d97b7c5bfcd0a3889fec5a4b0e
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-27 13:32:00 -0500

    Scripted auto-commit on change (2018-06-27 13:32:00) by gitwatch.sh

--- day02a/inheritance/Geometry.java
+++ day02a/inheritance/Geometry.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 public class Geometry {
    public static void main(String[] args) {
-      Circle c = new Circle();
-      System.out.println(c.area());
-      System.out.println(c.perimeter());
-      Rectangle r = new Rectangle();
-      System.out.println(r.area());
-      System.out.println(r.perimeter());
+      Scanner input = new Scanner(System.in);
+      System.out.print("(c)ircle or (r)ectangle? ");
+      String response = input.next();
+      if (response.equals("c")) {
+         Circle c = new Circle();
+         System.out.println(c.area());
+         System.out.println(c.perimeter());
+      } else {
+         Rectangle r = new Rectangle();
+         System.out.println(r.area());
+         System.out.println(r.perimeter());
+      }
    }
 }
