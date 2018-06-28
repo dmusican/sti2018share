da2bb9ab5a6d3bbdb6ebc67ea391647495fe2aec
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-28 10:28:29 -0500

    Scripted auto-commit on change (2018-06-28 10:28:28) by gitwatch.sh

--- day03a/TwoDGoodness.java
+++ day03a/TwoDGoodness.java
@@ -1,6 +1,8 @@
 public class TwoDGoodness {
   public static void main(String[] args) {
-    int[][] grid = new int[3][4];
+    int numRows = 4;
+    int numCols = 7;
+    int[][] grid = new int[numRows][numCols];
     
     for (int i=0; i < grid.length; i++) {
       for (int j=0; j < grid[0].length; j++) {
