c36c312ac007537089bb2d849cf242fdb4511b24
Dave Musicant on Skittles <meOnSkittles@carleton.edu>
2018-06-29 13:52:21 -0500

    Scripted auto-commit on change (2018-06-29 13:52:21) by gitwatch.sh

--- day04a/RecursionCoolness.java
+++ day04a/RecursionCoolness.java
@@ -16,8 +16,22 @@ public class RecursionCoolness {
     }
   }
   
+  public static int findMax(int[] arr, int start) {
+    if (start == arr.length-1) {
+      return arr[start];
+    } else {
+      int maxRest = findMax(arr, start + 1);
+      if (arr[start] > maxRest) {
+        return arr[start];
+      } else {
+        return maxRest;
+      }
+    }
+  
   public static void main(String[] args) {
     System.out.println(fact(5));
     System.out.println(reverse("friend"));
+    int[] mynums = {5,2,5,1,10,3};
+    System.out.println(findMax(mynums,0));
   }
 }
